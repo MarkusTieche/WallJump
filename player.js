3174cce448cd7793e9324d51933f3509b3cc8f27
MarkusTieche <tieche@gmx.com>
2025-02-25 17:04:17 +0100

    updated player

--- player.js
+++ player.js
@@ -4,18 +4,19 @@ class Player{
     {
         this.gravity = 1;
         this.halfSize = 98/2;
+        this.onWall = 0;
         this.body = document.getElementById("Player").cloneNode(true);
         this.body.setAttribute("fill", "#"+Math.floor(Math.random()*16777215).toString(16));//RANDOM COLOR1
         this.index = Index;
         this.reset(Position);
     }
 
-    reset(Position,Brain)
+    reset(Position)
     {
         this.position = {x:Position.x,y:Position.y-this.halfSize};
-        this.rotation = 0;
+        this.onWall = 0;
         this.velocity = {x:0,y:0};
-        this.body.setAttribute("transform","translate("+this.position.x+","+this.position.y+") rotate("+this.rotation+")");
+        this.body.setAttribute("transform","translate("+this.position.x+","+this.position.y+")");
         this.alive = true;
         this.lastRecord = 0;
         this.topPosition = 0;
@@ -35,46 +36,10 @@ class Player{
 
     move(DirectionX)
     {
-        this.velocity.y  =-10;
-        this.velocity.x  =25*DirectionX;
+        this.velocity.y  =-20;
+        this.velocity.x  =25*DirectionX*(this.onWall);
     }
 
-    //BRAIN
-    initBrain(Brain)
-    {
-        this.brain = Brain;
-        this.thinkTime = 15 ;
-        this.fitness = 0;
-        this.inputs = [];
-    }
-
-    think(ClosestSpike)
-    {
-        this.thinkTime --;
-        if(this.thinkTime+this.index <= 0)
-        {
-
-            this.thinkTime = 15 ;
-            //USE NORAMLIZED VALUES
-            this.inputs[0] = this.position.x/768;//PLAYER X POSITION
-            this.inputs[1] = (this.position.y+this.getLevelPart()*1280)/-1280;//PLAYER Y POSITION
-            this.inputs[2] = ClosestSpike.x/768; //NEXT SPIKE X POSITION 
-            this.inputs[3] = ClosestSpike.y/-1280;  //NEXT SPIKE Y POSITION 
-
-            var output = this.brain.predict(this.inputs);
-            if(output[0] > output[1])
-            {
-               this.move(-1)
-            }
-            else
-            {
-                this.move(1)
-            }
-        }
-    }
-
-    save(){}
-    //
 
     update()
     {
@@ -89,16 +54,23 @@ class Player{
         this.position.x = Math.min(Math.max(this.position.x, 768/2-230), 768/2+230);
         this.position.y += this.velocity.y*dt;
 
+        if(this.position.y = -this.halfSize)
+            {
+                console.log("dd")
+            }
+
         if(this.position.x <= 768/2-230 || this.position.x >= 768/2+230)
         {
             this.gravity = 0.1;
+            this.onWall = 1;
         }
         else
         {
             this.gravity = 1;
+            this.onWall = 0;
         }
 
-        this.body.setAttribute("transform","translate("+this.position.x+","+this.position.y+") rotate("+this.rotation+")");
+        this.body.setAttribute("transform","translate("+this.position.x+","+this.position.y+")");
 
         var topPosition = Math.min(this.topPosition,this.position.y+this.halfSize);
 
